ae32a81bc572df2b20b4a3c026dac41998a35a5f
EstherMx <esmaxi21@gmail.com>
2018-06-11 20:16:06 -0400

    connect and save data to mongoose

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+ 	app = express(),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose")
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-		{name: "Salmon Creek", image:"https://www.nps.gov/lacl/planyourvisit/images/Image-w-cred-cap_-1200w-_-Visit-Silver-Salmon-Creek-Page_2.jpg?maxwidth=1200&maxheight=1200&autorotate=false"},
-		{name: "Granite Hill", image:"http://www.americansouthwest.net/new_mexico/photographs700/aguirre-hill.jpg"}
-	]
+//schema
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+// 	{
+// 		name: "Granite", 
+// 		image:"http://www.americansouthwest.net/new_mexico/photographs700/aguirre-hill.jpg"
+// 	},function(err, campground){
+// 		if(err){
+// 			console.log(err);
+// 		}else{
+// 			console.log("newly created campground:");
+// 			console.log(campground);
+// 		}
+// 	});
+
 
 app.get("/", function(req,res){
 	res.render("landing");
 });
 
 app.get("/campgrounds", function(req,res){
-	res.render("campgrounds", {campgrounds:campgrounds});
+	//get all campgrounds from db
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("campgrounds", {campgrounds: allCampgrounds});
+		}
+	});
 });
 
 app.post("/campgrounds", function(req,res){
@@ -23,9 +49,14 @@ app.post("/campgrounds", function(req,res){
 	var name = req.body.name;
 	var image = req.body.image;
 	var newCampground = {name:name, image:image}
-	campgrounds.push(newCampground);
-	//redirect back to campgrounds page
-	res.redirect("/campgrounds");  //will redirect to the get request
+	//create a new campground and save to db
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		}else{
+			res.redirect("/campgrounds"); 
+		}
+	});
 });
 
 app.get("/campgrounds/new", function(req,res){
