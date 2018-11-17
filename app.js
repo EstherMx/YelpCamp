f0b1ec6742d70c47a5248a4ab89e303c7906dfe5
EstherMx <esmaxi21@gmail.com>
2018-11-17 01:24:39 -0500

    flash config

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
+    flash = require("connect-flash"),
     passport = require("passport"),
     LocalStrategy = require("passport-local"),
     methodOverride = require("method-override"),
-    flash = require("connect-flash"),
     Campground = require("./models/campground"),
     Comment = require("./models/comment"),
     User = require("./models/user"),
@@ -42,12 +42,14 @@ passport.deserializeUser(User.deserializeUser());
 //Creating a middleware for currentUser with "next" that can be used on any file
 app.use(function(req, res, next){
    res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
    next();
 });
 
 app.use(commentRoutes);
-app.use(campgroundRoutes);
 app.use(indexRoutes);
+app.use(campgroundRoutes);
 
 var port = process.env.PORT || 3000;
 app.listen(3000, function(){
