104e476865943ee994aef0cff7b0488b7c40ff63
EstherMx <esmaxi21@gmail.com>
2018-11-17 01:25:11 -0500

    flash 'error' if the user is not the author

--- middleware/index.js
+++ middleware/index.js
@@ -17,18 +17,25 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next){
     if(req.isAuthenticated()){
         Campground.findById(req.params.id, function(err, foundCampground){
            if(err){
+                req.flash("error", "Campground not found");
                 res.redirect("back"); //back to the previous page
             }else{
+                if(!foundCampground){
+                    req.flash("error", "Item not found.");
+                    return res.redirect("back");
+                }
                 //does user own the campground?
                 //check if the author id = the user id
                 if(foundCampground.author.id.equals(req.user._id)){
                    next();
                 }else{
+                    req.flash("error", "You don't have permission to do that");
                     res.redirect("back");
                 }
             }
         });
     }else{
+        req.flash("error", "Please Login First!");
         res.redirect("back"); 
     }
 }
@@ -44,11 +51,13 @@ middlewareObj.checkCommentOwnership = function (req, res, next){
                 if(foundComment.author.id.equals(req.user._id)){
                    next();
                 }else{
+                    req.flash("error", "You don't have permission to do that!");
                     res.redirect("back");
                 }
             }
         });
     }else{
+        req.flash("error", "Please, login first!")
         res.redirect("back"); 
     }
 }
